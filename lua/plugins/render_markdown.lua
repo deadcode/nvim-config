9b5543c5ba77683578cf00411c38353a4ab31594
Sandeep Dahiya <sandeep.dahiya@gigamon.com>
2026-02-03 11:14:22 -0800

    Remove broken plugins

--- lua/plugins/render_markdown.lua
+++ lua/plugins/render_markdown.lua
@@ -7,5 +7,5 @@ return {
     }
     rmd.setup(opts)
   end,
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+  dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
 }
